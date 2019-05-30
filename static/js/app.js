b2286f8f0a1256b17566f80a79ae694b02e92a0d
unknown <obaid8712@gmail.com>
2019-05-30 12:13:17 -0400

    Completed

--- static/js/app.js
+++ static/js/app.js
@@ -29,24 +29,24 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-    url=`/samples/${sample}`
-    d3.json(url).then(function(data){
+  url=`/samples/${sample}`
+  d3.json(url).then(function(data){
     // @TODO: Build a Pie Chart
-     // Create dict data into a new list for several data
-     var listData = [];
-     lcount = data.otu_ids.length;
-     for (i=0; i<lcount; i++){
-       var eachData = {};
-       Object.entries(data).forEach(([key,value])=>{
-         eachData[key] = value[i];
-       });
-       listData.push(eachData);
-     };
-      //Sort values for top 10 and slice() to grab the top 10 sample_values,
-      listData.sort(function(a,b){
-        return parseFloat(b.sample_values) - parseFloat(a.sample_values)
+    // Create dict data into a new list for several data
+    var listData = [];
+    lcount = data.otu_ids.length;
+    for (i=0; i<lcount; i++){
+      var eachData = {};
+      Object.entries(data).forEach(([key,value])=>{
+        eachData[key] = value[i];
       });
-      top10Data = listData.slice(0,10);
+      listData.push(eachData);
+    };
+    //Sort values for top 10 and slice() to grab the top 10 sample_values,
+    listData.sort(function(a,b){
+      return parseFloat(b.sample_values) - parseFloat(a.sample_values)
+    });
+    top10Data = listData.slice(0,10);
     // HINT: You will need to use 
     // otu_ids, and labels (10 each).
     var pie = [{
@@ -55,8 +55,7 @@ function buildCharts(sample) {
       type : "pie"
     }];
 
-    var layout = {
-      //title: '<b>The Top 10</b>',
+    var layout = {      
       height: 450,
       width: 450
     };
@@ -64,17 +63,30 @@ function buildCharts(sample) {
     Plotly.newPlot("pie",pie,layout);
 
     // @TODO: Build a Bubble Chart using the sample data
-    });  
+    var Bubble = [{
+      x : data.otu_ids,
+      y : data.sample_values,
+      marker :{
+        size : data.sample_values,
+        color : data.otu_ids
+      },
+      text : data.otu_labels,
+      mode : "markers"
+    }];  
+  
+    Plotly.newPlot('bubble',Bubble);  
+
+});  
     
 }
 
+// @TODO: Build a Guage function using the sample data
 function buildGauge(sample){
-  // Enter a speed between 0 and 180
-var level = 120;
+// Enter a sample and create level between 0 and 180
+var level = sample * 20-10;
 
-// Trig to calc meter point
-var degrees = 180 - level,
-     radius = .5;
+// Scrub meter point
+var degrees = 180 - level , radius = .5;
 var radians = degrees * Math.PI / 180;
 var x = radius * Math.cos(radians);
 var y = radius * Math.sin(radians);
@@ -90,21 +102,25 @@ var path = mainPath.concat(pathX,space,pathY,pathEnd);
 var data = [{ type: 'scatter',
    x: [0], y:[0],
     marker: {size: 28, color:'850000'},
+    //marker: {size: 28, color:'greys'},
     showlegend: false,
-    name: 'speed',
-    text: level,
+    name: 'WFREQ',
+    text: sample,
     hoverinfo: 'text+name'},
-  { values: [50/6, 50/6, 50/6, 50/6, 50/6, 50/6, 50],
+
+{ values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9,50/9,50],
   rotation: 90,
-  text: ['TOO FAST!', 'Pretty Fast', 'Fast', 'Average',
-            'Slow', 'Super Slow', ''],
+  text: ['8-9','7-8','6-7','5-6', '4-5', '3-4', '2-3',
+            '1-2', '0-1', ''],
   textinfo: 'text',
   textposition:'inside',
-  marker: {colors:['rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
-                         'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
-                         'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+  marker: {colors:['rgba(14, 120, 0, .5)', 'rgba(25, 134, 50, .5)',
+                         'rgba(50, 150, 80, .5)', 'rgba(100, 165, 105, .5)',
+                         'rgba(125, 180, 130, .5)', 'rgba(150, 195, 155, .5)',
+                         'rgba(175, 210, 180, .5)', 'rgba(200, 225, 205, .5)',
+                         'rgba(225, 240, 230, .5)', 
                          'rgba(255, 255, 255, 0)']},
-  labels: ['151-180', '121-150', '91-120', '61-90', '31-60', '0-30', ''],
+  labels: ['161-180', '141-160', '121-140', '101-120', '81-100', '61-80', '41-60','21-40','0-20',''],
   hoverinfo: 'label',
   hole: .5,
   type: 'pie',
@@ -120,9 +136,9 @@ var layout = {
         color: '850000'
       }
     }],
-  title: '<b>Gauge</b> <br> Speed 0-100',
-  height: 1000,
-  width: 1000,
+  title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
+  height: 500,
+  width: 500,
   xaxis: {zeroline:false, showticklabels:false,
              showgrid: false, range: [-1, 1]},
   yaxis: {zeroline:false, showticklabels:false,
